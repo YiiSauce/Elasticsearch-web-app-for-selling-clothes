520a8a6b488e01c14bc570e045af10b6761ffb4f
LaSauce <youssefyouss321@gmail.com>
2023-04-25 18:09:40 +0100

    Here

--- src/main/java/com/example/demo/configuration/Config.java
+++ src/main/java/com/example/demo/configuration/Config.java
@@ -6,8 +6,12 @@ import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 
 //@Configuration
@@ -27,21 +31,15 @@ import org.springframework.context.annotation.Configuration;
 //}
 
 @Configuration
+@EnableElasticsearchRepositories(basePackages = "com.example.demo.repository")
 public class Config {
-        @Bean
-        public RestClient getRestClient() {
-            RestClient restClient = RestClient.builder(
-                    new HttpHost("localhost", 9200)).build();
-            return restClient;
-        }
-        @Bean
-        public ElasticsearchTransport getElasticsearchTransport() {
-            return new RestClientTransport(
-                    getRestClient(), new JacksonJsonpMapper());
-        }
-        @Bean
-        public ElasticsearchClient getElasticsearchClient(){
-            ElasticsearchClient client = new ElasticsearchClient(getElasticsearchTransport());
-            return client;
-        }
+    @Bean
+    public RestHighLevelClient client() {
+        return new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http")));
+    }
+    @Bean
+    public ElasticsearchOperations elasticsearchTemplate() {
+        return new ElasticsearchRestTemplate(client());
+    }
     }
